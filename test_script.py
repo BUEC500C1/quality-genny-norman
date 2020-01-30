25f116793633a334b944ad19eca11d7d6730fd69
Genny Norman <gnorman@bu.edu>
2020-01-29 22:12:44 -0500

    add tests

--- test_script.py
+++ test_script.py
@@ -1,4 +1,22 @@
 from arabicToRomanConv import conversion
 
-def test_init():
+def test_basic():
+    assert conversion(55) == "LV"
+
+def test_alpha():
+    assert conversion('asdjf') == ""
+
+def test_zero():
+    assert conversion(0) == ""
+
+def test_fours():
     assert conversion(14) == "XIV"
+
+def test_nines():
+    assert conversion(99) == "XCIX"
+
+def test_decimal():
+    assert conversion(12.3) == ""
+
+def test_negative():
+    assert conversion(-4) == ""
